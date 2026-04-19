0b903794cdddcae6fb624a7fbf3995a67d081202
Celeste Parlow <s0599367@htw.berlin.de>
2026-04-19 03:54:32 +0200

    generated backend

---
+++ src/main/java/de/htw_berlin/beMindful/BeMindfulApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.beMindful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMindfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMindfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/beMindful/BeMindfulApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.beMindful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMindfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
