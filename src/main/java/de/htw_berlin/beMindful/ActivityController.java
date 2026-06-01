24b387001ae21edc7d36646578517ea9a37d09b6
Celeste Parlow <s0599367@htw.berlin.de>
2026-06-01 12:39:36 +0200

    crossorigin - render?

--- src/main/java/de/htw_berlin/beMindful/ActivityController.java
+++ src/main/java/de/htw_berlin/beMindful/ActivityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin (origins = "https://bemindful-frontend.onrender.com")
 public class ActivityController {
 
     @GetMapping("/activities")
